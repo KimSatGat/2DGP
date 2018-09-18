995f428d664e27f4f52bd98dd1b973b7e3c4cf99
Bruce <cbaal@korea.com>
2018-09-18 18:38:25 +0900

    이동 알고리즘 완성
    체크포인트1 부터 10까지 이동하는 함수를 각각 구현하여 반복 재생한다.

--- Drills/Drill-05/Drill05.py
+++ Drills/Drill-05/Drill05.py
@@ -6,8 +6,48 @@ grass = load_image('grass.png')
 character = load_image('character.png')
 
 
-def MoveCheckPoint():
+def Move1to2():
+    pass
 
+def Move2to3():
+    pass
+
+def Move3to4():
+    pass
+
+def Move4to5():
+    pass
+
+def Move5to6():
+    pass
+
+def Move6to7():
+    pass
+
+def Move7to8():
+    pass
+
+def Move8to9():
+    pass
+
+def Move9to10():
+    pass
+
+def Move10to1():
+    pass
+
+
+def MoveCheckPoint():
+    Move1to2()
+    Move2to3()
+    Move3to4()
+    Move4to5()
+    Move5to6()
+    Move6to7()
+    Move7to8()
+    Move8to9()
+    Move9to10()
+    Move10to1()
 
 
 while (True):
