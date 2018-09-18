e93c2952b5ee287dc283d019f6c3f53a6f1aea47
Bruce <cbaal@korea.com>
2018-09-18 18:42:36 +0900

    체크포인트 1->2로 가는 기능 구현
    Move1to2 함수 구현 완료

--- Drills/Drill-05/Drill05.py
+++ Drills/Drill-05/Drill05.py
@@ -1,13 +1,29 @@
 from pico2d import *
-import math
+
 open_canvas()
 
 grass = load_image('grass.png')
-character = load_image('character.png')
+character = load_image('animation_sheet.png')
 
 
 def Move1to2():
-    pass
+    x = 203
+    y = 535
+    moveX = (132 - x) / 20
+    moveY = (243 - y) / 20
+    cnt = 0
+    frame = 0
+    while (cnt < 20):
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 0, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        x += moveX
+        y += moveY
+        cnt = cnt + 1
+        delay(0.05)
+        get_events()
 
 def Move2to3():
     pass
@@ -39,17 +55,20 @@ def Move10to1():
 
 def MoveCheckPoint():
     Move1to2()
-    Move2to3()
-    Move3to4()
-    Move4to5()
-    Move5to6()
-    Move6to7()
-    Move7to8()
-    Move8to9()
-    Move9to10()
-    Move10to1()
+    #Move2to3()
+    #Move3to4()
+    #Move4to5()
+    #Move5to6()
+    #Move6to7()
+    #Move7to8()
+    #Move8to9()
+    #Move9to10()
+    #Move10to1()
 
 
 while (True):
     MoveCheckPoint()
+
+
+
 close_canvas()
