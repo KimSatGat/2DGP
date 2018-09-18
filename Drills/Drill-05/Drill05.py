1d1b01b42c72dc77aa71cd564ea59ba36f215717
Bruce <cbaal@korea.com>
2018-09-18 18:25:24 +0900

    make_circle 함수 완성
    원 그리기 기능

--- Drills/Drill-05/Drill05.py
+++ Drills/Drill-05/Drill05.py
@@ -1,5 +1,5 @@
 from pico2d import *
-
+import math
 open_canvas()
 
 grass = load_image('grass.png')
@@ -33,7 +33,6 @@ def move_left():
         character.draw_now(x, y)
         x -= 2
         delay(0.01)
-    pass
 
 
 def move_down():
@@ -56,14 +55,25 @@ def move_left_to_center():
 
 
 def make_rectangle():
-    #move_from_center_to_right()
-    #move_up()
-    #move_left()
-    #move_down()
+    move_from_center_to_right()
+    move_up()
+    move_left()
+    move_down()
     move_left_to_center()
 
 def make_circle():
-    pass
+    cx, cy, r  = 800 // 2, 600 // 2, (600-180) // 2
+    degree = -90
+    while degree < 270:
+        radian = math.radians(degree)
+        x = cx + r * math.cos(radian)
+        y = cy + r * math.sin(radian)
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        degree = degree + 1
+        delay(0.01)
+
 
 while True:
     make_rectangle()
