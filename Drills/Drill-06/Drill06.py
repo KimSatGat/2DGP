c22a7bd45e4d2f21fa54602787f409fdb27396f1
Bruce <cbaal@korea.com>
2018-09-20 19:10:33 +0900

    이동하는 함수 구현
    Move 함수를 통해서 원하는 위치로 이동 한다

--- Drills/Drill-06/Drill06.py
+++ Drills/Drill-06/Drill06.py
@@ -16,6 +16,8 @@ def handle_events():
             running = False
         elif event.type == SDL_MOUSEMOTION:
             mouseX, mouseY = event.x, KPU_HEIGHT - 1 - event.y
+        elif SDL_BUTTON_LEFT:
+            Move()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
@@ -25,13 +27,53 @@ def Idle_Right():
         character.clip_draw(frame * 100, 100 * 3, 100, 100, x, y)
         frame = (frame + 1) % 8
 
-
 def Idle_Left():
     global frame
     global x, y
     character.clip_draw(frame * 100, 100 * 2, 100, 100, x, y)
     frame = (frame + 1) % 8
 
+def Move():
+    global x, y, mouseX, mouseY
+    global dir
+    global frame
+    if(mouseX - x > 0):
+        dir = 1
+    else:
+        dir = -1
+    if(dir > 0):
+        moveX = (mouseX - x) / 20
+        moveY = (mouseY - y) / 20
+        cnt = 0
+        frame = 0
+        while (cnt < 20):
+            clear_canvas()
+            kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+            character.clip_draw(frame * 100, 100, 100, 100, x, y)
+            mouse.draw(mouseX, mouseY)
+            update_canvas()
+            frame = (frame + 1) % 8
+            x += moveX
+            y += moveY
+            cnt = cnt + 1
+            delay(0.05)
+    else:
+        moveX = (mouseX - x) / 20
+        moveY = (mouseY - y) / 20
+        cnt = 0
+        frame = 0
+        while (cnt < 20):
+            clear_canvas()
+            kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+            character.clip_draw(frame * 100, 0, 100, 100, x, y)
+            mouse.draw(mouseX, mouseY)
+            update_canvas()
+            frame = (frame + 1) % 8
+            x += moveX
+            y += moveY
+            cnt = cnt + 1
+            delay(0.05)
+            handle_events()
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
@@ -42,13 +84,14 @@ running = True
 mouseX, mouseY = KPU_WIDTH // 2, KPU_HEIGHT // 2
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 frame = 0
+dir = 0
 hide_cursor()
 
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
     mouse.draw(mouseX, mouseY)
-    Idle_Left()
+    Idle_Right()
     update_canvas()
 
 
