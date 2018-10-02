d427324b676d143e5ef2dfc486404e93c163dc46
Bruce <cbaal@korea.com>
2018-10-02 18:48:27 +0900

    Drill 07 이벤트 함수 구현
    handle_events 이벤트 함수를 구현하여 종료 기능 추가

--- Drills/Drill-07/Drill07.py
+++ Drills/Drill-07/Drill07.py
@@ -12,7 +12,15 @@ dir = 0         #방향
 points = [(random.randint(0, 1280), random.randint(0, 1024)) for i in range(pointNumber)]
 
 def handle_events():
-    pass
+    global  running
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
 def MoveRandomPoints(p1, p2):
     pass
 def DetermineDirection(p1,p2):
