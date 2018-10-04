79c7980d0ebc902ea36f8b99dcc2256aecc97ce6
Bruce <cbaal@korea.com>
2018-10-04 18:31:53 +0900

    Drill08-1 완성
    부드러운 점찍기 기능

--- Drills/Drill-08/Drill08-1.py
+++ Drills/Drill-08/Drill08-1.py
@@ -58,7 +58,7 @@ def draw_curve_3_points(p1, p2, p3):
     draw_big_point(p2)
     draw_big_point(p3)
 
-    for i in range(0, 100, 2):
+    for i in range(0, 50, 2):
         t = i / 100
         x = (2*t**2 - 3*t + 1) * p1[0] + (-4*t**2 + 4*t) * p2[0] + (2*t**2 - t) * p3[0]
         y = (2*t**2 - 3*t + 1) * p1[1] + (-4*t**2 + 4*t) * p2[1] + (2*t**2 - t) * p3[1]
@@ -95,11 +95,26 @@ def draw_curve_4_points(p1, p2, p3, p4):
         draw_point((x, y))
     draw_point(p4)
 
+def draw_curve(p1, p2, p3, p4):
+    draw_big_point(p1)
+    draw_big_point(p2)
+    draw_big_point(p3)
+    draw_big_point(p4)
+
+    for i in range(0, 100, 2):
+        t = i / 100
+        x = ((-t**3 + 2*t**2 - t)*p1[0] + (3*t**3 - 5*t**2 + 2)*p2[0] + (-3*t**3 + 4*t**2 + t)*p3[0] + (t**3 - t**2)*p4[0])/2
+        y = ((-t**3 + 2*t**2 - t)*p1[1] + (3*t**3 - 5*t**2 + 2)*p2[1] + (-3*t**3 + 4*t**2 + t)*p3[1] + (t**3 - t**2)*p4[1])/2
+        draw_point((x, y))
 
 
 points = [(-300, 200), (400, 350), (300, -300), (-200, -200)]
 
 prepare_turtle_canvas()
-
+while True:
+    draw_curve((-200, -200), (-300, 200), (400, 350), (300, -300))
+    draw_curve((-300, 200), (400, 350), (300, -300), (-200, -200))
+    draw_curve((400, 350), (300, -300), (-200, -200), (-300, 200))
+    draw_curve((300, -300), (-200, -200), (-300, 200), (400, 350))
 
 turtle.done()
