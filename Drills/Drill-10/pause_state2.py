3b063190c585263aa6130422f6853746359474c3
Bruce <cbaal@korea.com>
2018-10-11 21:11:59 +0900

    일시정지 시 게임 이미지도 표시
    main_state를 import 해서 pause_state2에서 호출

--- Drills/Drill-10/pause_state2.py
+++ Drills/Drill-10/pause_state2.py
@@ -1,4 +1,5 @@
 import game_framework
+import main_state
 from pico2d import *
 
 name = "PauseSate"
@@ -6,7 +7,7 @@ image = None
 
 def enter():
     global image
-    image = load_image('pause.png')
+    image = load_image('pause2.png')
 
 def exit():
     global image
@@ -18,6 +19,8 @@ def update():
 def draw():
     global image
     clear_canvas()
+    main_state.boy.draw()
+    main_state.grass.draw()
     image.draw(400, 300)
     update_canvas()
 
@@ -26,6 +29,10 @@ def handle_events():
     for event in events:
         if (event.type, event.key) == (SDL_KEYDOWN, SDLK_p):
             game_framework.pop_state()
+        elif event.type == SDL_QUIT:
+                game_framework.quit()
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
+                game_framework.quit()
 
 def pause():
     pass
