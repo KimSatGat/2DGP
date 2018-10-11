95b15421f1dac04eedf9a83a31b681b2ce5428ed
Bruce <cbaal@korea.com>
2018-10-11 20:29:30 +0900

    프레임워크 정의
    프레임워크에 맞는 함수 정의

--- Drills/Drill-10/pause_state1.py
+++ Drills/Drill-10/pause_state1.py
@@ -3,3 +3,24 @@ from pico2d import *
 
 name = "PauseSate"
 image = None
+
+def enter():
+    pass
+
+def exit():
+    pass
+
+def update():
+    pass
+
+def draw():
+    pass
+def handle_events():
+    pass
+
+def pause():
+    pass
+
+def resume():
+    pass
+
