7269e0906a2048c9682dbf2ff60fb647757a7d3b
Bruce <cbaal@korea.com>
2018-10-11 20:31:36 +0900

    handle_events 함수 정의
    p 키를 누르면 이전 상태로 되돌아가는 기능

--- Drills/Drill-10/pause_state1.py
+++ Drills/Drill-10/pause_state1.py
@@ -22,7 +22,10 @@ def draw():
     update_canvas()
 
 def handle_events():
-    pass
+    events = get_events()
+    for event in events:
+        if (event.type, event.key) == (SDL_KEYDOWN, SDLK_p):
+            game_framework.pop_state()
 
 def pause():
     pass
