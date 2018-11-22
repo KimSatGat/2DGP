d5178ab34752a26f9ae552f5d8c969d9a0607d1a
Bruce <cbaal@korea.com>
2018-11-22 15:26:47 +0900

    Ball 좌표 월드좌표로 수정

--- Drills/Drill-14/ball.py
+++ Drills/Drill-14/ball.py
@@ -2,6 +2,7 @@ import random
 from pico2d import *
 import game_world
 import game_framework
+import main_state
 
 class Ball:
     image = None
@@ -10,6 +11,8 @@ class Ball:
         if Ball.image == None:
             Ball.image = load_image('ball21x21.png')
         self.x, self.y, self.fall_speed = random.randint(0, 1600-1), random.randint(0, 600-1), 0
+        self.velocityX = 0
+        self.velocityY = 0
 
     def get_bb(self):
         return self.x - 10, self.y - 10, self.x + 10, self.y + 10
@@ -19,5 +22,7 @@ class Ball:
         draw_rectangle(*self.get_bb())
 
     def update(self):
-        self.y -= self.fall_speed * game_framework.frame_time
-
+        self.velocityX = main_state.boy.x_velocity
+        self.velocityY = main_state.boy.y_velocity
+        self.x -= self.velocityX * game_framework.frame_time
+        self.y -= self.velocityY * game_framework.frame_time
